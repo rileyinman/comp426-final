4b10304d86430c0077fae54731e14c7f5ef8416c
Jasper Christie <jasp@live.unc.edu>
2020-10-12 23:45:40 -0400

    added some folders

---
+++ levels/level0/floor.js
@@ -0,0 +1,20 @@
+// logic for player navigating floor
+
+
+// floor layout
+const floorDesign = [
+    [1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1]
+];
+
+
+// function for floor setup
+function Floor({ design = floorDesign, start = [3, 7], door = [5, 1], squareWidth = 90, squareHeight = 90}) {
+
+}
