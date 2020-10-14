debe04897baa3250305c38233927cee482fa6154
s-fin <finleysun@Finleys-MacBook-Air-2.local>
2020-10-14 16:09:36 -0400

    basic map

--- levels/level0/floor.js
+++ levels/level0/floor.js
@@ -3,18 +3,31 @@
 
 // floor layout
 const floorDesign = [
-    [1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1]
+    [1, 1, 0, 1, 1, 1, 1, 1],
+    [1, 0, 0, 1, 1, 1, 1, 1],
+    [1, 0, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0, 1, 1, 1],
+    [1, 1, 1, 1, 0, 1, 1, 1],
+    [1, 1, 1, 1, 0, 1, 1, 1],
+    [1, 1, 1, 0, 0, 1, 1, 1],
+    [1, 1, 1, 0, 1, 1, 1, 1]
 ];
 
 
-// function for floor setup
-function Floor({ design = floorDesign, start = [3, 7], door = [5, 1], squareWidth = 90, squareHeight = 90}) {
 
-}
+   var c = document.getElementById("c")
+   var canvas = c.getContext("2d");
+ 
+
+    for (let i = 0; i < floorDesign.length; i++) {
+        for (let j = 0; j < floorDesign[0].length; j++) {
+            if (floorDesign[i][j] == 1) {
+              canvas.fillStyle = "#ff66d9";
+               canvas.fillRect(j*90, i*90, 90, 90);
+            } else {
+                canvas.fillStyle = "#96e38f";
+                canvas.fillRect(j*90, i*90, 90, 90);
+            }
+        }
+    }
+
