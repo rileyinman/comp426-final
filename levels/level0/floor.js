9474508e7b0a62b531c6d783deae99f8aded00c7
Riley Inman <rileyminman@gmail.com>
2020-10-14 22:43:40 -0400

    Added basic character sprite, created some abstractions
    Note: A lot of this should be moved out of the level0 folder and made
    accessible to other files in order to make things as extensible as
    possible. This is just a start to making things work more easily.

--- levels/level0/floor.js
+++ levels/level0/floor.js
@@ -1,33 +1,71 @@
 // logic for player navigating floor
 
+function Tile(color) {
+    this.color = color;
+}
+
+const grassTile = new Tile('#00b543')
+const roadTile = new Tile('#a28879')
 
 // floor layout
-const floorDesign = [
-    [1, 1, 0, 1, 1, 1, 1, 1],
-    [1, 0, 0, 1, 1, 1, 1, 1],
-    [1, 0, 1, 1, 1, 1, 1, 1],
-    [1, 0, 0, 0, 0, 1, 1, 1],
-    [1, 1, 1, 1, 0, 1, 1, 1],
-    [1, 1, 1, 1, 0, 1, 1, 1],
-    [1, 1, 1, 0, 0, 1, 1, 1],
-    [1, 1, 1, 0, 1, 1, 1, 1]
+const level1Layout = [
+    [grassTile, grassTile, roadTile,  grassTile, grassTile, grassTile, grassTile, grassTile],
+    [grassTile, roadTile,  roadTile,  grassTile, grassTile, grassTile, grassTile, grassTile],
+    [grassTile, roadTile,  grassTile, grassTile, grassTile, grassTile, grassTile, grassTile],
+    [grassTile, roadTile,  roadTile,  roadTile,  roadTile,  grassTile, grassTile, grassTile],
+    [grassTile, grassTile, grassTile, grassTile, roadTile,  grassTile, grassTile, grassTile],
+    [grassTile, grassTile, grassTile, grassTile, roadTile,  grassTile, grassTile, grassTile],
+    [grassTile, grassTile, grassTile, roadTile,  roadTile,  grassTile, grassTile, grassTile],
+    [grassTile, grassTile, grassTile, roadTile,  grassTile, grassTile, grassTile, grassTile]
 ];
 
+function Level(layout, startCoords) {
+    this.layout = layout;
+    this.startCoords = startCoords;
+    this.height = this.layout.length;
+    this.width = this.layout[0].length;
+}
 
+const level1 = new Level(level1Layout, [7, 3]);
 
-   var c = document.getElementById("c")
-   var canvas = c.getContext("2d");
- 
+const arrayEquals = (a, b) => {
+    return Array.isArray(a) &&
+        Array.isArray(b) &&
+        a.length === b.length &&
+        a.every((val, index) => val === b[index]);
+}
 
-    for (let i = 0; i < floorDesign.length; i++) {
-        for (let j = 0; j < floorDesign[0].length; j++) {
-            if (floorDesign[i][j] == 1) {
-              canvas.fillStyle = "#ff66d9";
-               canvas.fillRect(j*90, i*90, 90, 90);
-            } else {
-                canvas.fillStyle = "#96e38f";
-                canvas.fillRect(j*90, i*90, 90, 90);
+function drawLevel(level) {
+    const canvas = document.getElementsByClassName('game-container')[0]
+    const ctx = canvas.getContext("2d");
+    ctx.imageSmoothingEnabled = false;
+
+    const playerSprite = new Image();
+    playerSprite.src = '../../assets/player.png';
+
+    for (let i = 0; i < level.height; i++) {
+        for (let j = 0; j < level.width; j++) {
+            ctx.beginPath();
+            ctx.fillStyle = level.layout[i][j].color;
+            ctx.fillRect(j*90, i*90, 90, 90);
+            if (arrayEquals([i, j], level.startCoords)) {
+                playerSprite.onload = function() {
+                    ctx.drawImage(
+                        playerSprite,
+                        j*90 + 12,
+                        i*90 + 10,
+                        64,
+                        64
+                    )
+                }
             }
         }
     }
+}
 
+document.addEventListener(
+    'DOMContentLoaded',
+    function() {
+        drawLevel(level1);
+    }
+);
