184410cc499f712206b2726516ab300e82b16aad
Riley Inman <rileyminman@gmail.com>
2020-11-26 22:02:35 -0500

    Temporarily revert protection of game route

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import { ProtectedRoute } from './auth';
-import { Game, Home, Login, Profile } from './views';
+import { Game, Home, Login, Profile, Register } from './views';
 import { Loading, NavBar } from './components';
 
 import './App.scss';
@@ -20,7 +20,9 @@ const App = () => {
       <Switch>
         <Route path='/' exact component={Home}/>
         <Route path='/login' component={Login}/>
-        <ProtectedRoute path='/game' component={Game}/>
+        <Route path='/register' component={Register}/>
+        <Route path='/game' component={Game}/>
+        {/* <ProtectedRoute path='/game' component={Game}/> */}
         <ProtectedRoute path='/profile' component={Profile}/>
       </Switch>
     </>
