4b455e7d55c4744e255b74e858f07b06ce40d57f
Riley Inman <rileyminman@gmail.com>
2020-11-27 22:28:16 -0500

    Delete hasKey helper

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,5 +1,4 @@
 import { enumContains } from './Enum';
 import { indexOf2d } from './2dArray';
-import { hasKey } from './HasKey';
 
-export { enumContains, indexOf2d, hasKey };
+export { enumContains, indexOf2d };
