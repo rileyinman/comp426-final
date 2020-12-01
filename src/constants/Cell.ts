fd6cac593824bc9038d0b550b85ecd9466f558a8
Riley Inman <rileyminman@gmail.com>
2020-12-01 03:35:57 -0500

    Change double quotes to single for consistency

--- src/constants/Cell.ts
+++ src/constants/Cell.ts
@@ -1,34 +1,34 @@
 enum Item {
-  KEY1 = "key1",
-  KEY2 = "key2",
-  KEY3 = "key3",
-  KEY4 = "key4",
-  KEY5 = "key5"
+  KEY1 = 'key1',
+  KEY2 = 'key2',
+  KEY3 = 'key3',
+  KEY4 = 'key4',
+  KEY5 = 'key5'
 }
 
 enum Obstacle {
-  WALL = "wall",
-  DOOR1 = "door1",
-  DOOR2 = "door2",
-  DOOR3 = "door3",
-  DOOR4 = "door4",
-  DOOR5 = "door5"
+  WALL = 'wall',
+  DOOR1 = 'door1',
+  DOOR2 = 'door2',
+  DOOR3 = 'door3',
+  DOOR4 = 'door4',
+  DOOR5 = 'door5'
 }
 
 enum Floor {
-  DEFAULT = "floor",
-  EXIT = "exit"
+  DEFAULT = 'floor',
+  EXIT = 'exit'
 }
 
 enum Empty  {
-  DEFAULT = "empty"
+  DEFAULT = 'empty'
 }
 
 enum Player {
-  PLAYER1 = "player1",
-  PLAYER2 = "player2",
-  PLAYER3 = "player3",
-  PLAYER4 = "player4"
+  PLAYER1 = 'player1',
+  PLAYER2 = 'player2',
+  PLAYER3 = 'player3',
+  PLAYER4 = 'player4'
 }
 
 export { Empty, Floor, Item, Obstacle, Player };
